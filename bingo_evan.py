06cd1e98336b1368d289532b7ca9747a15022ddf
Evan Lepolt <evan.lepolt@gmail.com>
2019-07-29 13:59:32 -0600

    Evans contribution to the bingo song

---
+++ bingo_evan.py
@@ -0,0 +1,60 @@
+# Quick and dirty bingo song.
+
+# This line splits the word 'bingo' up into an array of letters, ['b', 'i', 'n', 'g', 'o']
+word_list = list("bingo")
+
+# First time through just print the hardcoded song
+print("There was a farmer who had a dog and bingo was his name-o")
+# Loop through the spelling three times
+for j in range(3):
+  # By joining the array with a hyphen
+  print("-".join(word_list))
+
+# Initialze our index variable at -1
+i = -1
+# While index is less than word_list length minus 1 (because of indexing purposes)
+while i < len(word_list) - 1:
+  # Print the song
+  print("\nThere was a farmer who had a dog and bingo was his name-o")
+  # Increment our index variable
+  i = i+1
+  # Replace that index of the array with an exclamation point
+  word_list[i] = "!"
+  # Print the spelling three times
+  for j in range(3):
+    print("-".join(word_list))
+
+
+"""
+Final Output:
+
+There was a farmer who had a dog and bingo was his name-o
+b-i-n-g-o
+b-i-n-g-o
+b-i-n-g-o
+
+There was a farmer who had a dog and bingo was his name-o
+!-i-n-g-o
+!-i-n-g-o
+!-i-n-g-o
+
+There was a farmer who had a dog and bingo was his name-o
+!-!-n-g-o
+!-!-n-g-o
+!-!-n-g-o
+
+There was a farmer who had a dog and bingo was his name-o
+!-!-!-g-o
+!-!-!-g-o
+!-!-!-g-o
+
+There was a farmer who had a dog and bingo was his name-o
+!-!-!-!-o
+!-!-!-!-o
+!-!-!-!-o
+
+There was a farmer who had a dog and bingo was his name-o
+!-!-!-!-!
+!-!-!-!-!
+!-!-!-!-!
+"""
